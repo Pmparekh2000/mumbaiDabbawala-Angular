18a1e84ae718db971d7954a29d748dcc136e40da
Pmparekh2000 <pmp.tech2000@gmail.com>
2020-03-15 13:40:01 +0530

    Routing added with 4 components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
-import { NgModule } from '@angular/core';
+import { UserRegisterComponent } from './user-register/user-register.component';
+import { UserLoginComponent } from './user-login/user-login.component';
+import { DabbawalaRegisterComponent } from './dabbawala-register/dabbawala-register.component';
+import { DabbawalaLoginComponent } from './dabbawala-login/dabbawala-login.component';
+import { NgModule} from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'dabbawala-login',component: DabbawalaLoginComponent},
+  {path: 'dabbawala-register',component: DabbawalaRegisterComponent},
+  {path: 'user-login',component: UserLoginComponent},
+  {path: 'user-register',component: UserRegisterComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [DabbawalaLoginComponent, DabbawalaRegisterComponent, UserLoginComponent, UserRegisterComponent];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents} from './app-routing.module';
 import { AppComponent } from './app.component';
-import { UserLoginComponent } from './user-login/user-login.component';
-import { UserRegisterComponent } from './user-register/user-register.component';
-import { DabbawalaRegisterComponent } from './dabbawala-register/dabbawala-register.component';
-import { DabbawalaLoginComponent } from './dabbawala-login/dabbawala-login.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserLoginComponent,
-    UserRegisterComponent,
-    DabbawalaRegisterComponent,
-    DabbawalaLoginComponent
+    routingComponents
   ],
   imports: [
     BrowserModule,
