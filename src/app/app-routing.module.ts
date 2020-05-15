1fa4d4e4083e72cb4a8d40a71d0712e32eec784c
Chirag Rana <42936626+ChiragJRana@users.noreply.github.com>
2020-05-15 14:15:38 +0530

    Deleted Unwanted components and modified footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,9 +6,7 @@ import { TedTalksComponent } from './ted-talks/ted-talks.component';
 import { DabbaServiceComponent } from './dabba-service/dabba-service.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { HomeComponent } from './home/home.component';
-import { UserRegisterComponent } from './user-register/user-register.component';
 import { UserLoginComponent } from './user-login/user-login.component';
-import { DabbawalaRegisterComponent } from './dabbawala-register/dabbawala-register.component';
 import { DabbawalaLoginComponent } from './dabbawala-login/dabbawala-login.component';
 import { NgModule} from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
@@ -17,9 +15,7 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
 const routes: Routes = [
   {path: '', redirectTo:'/home', pathMatch:'full'},
   {path: 'dabbawala-login',component: DabbawalaLoginComponent},
-  {path: 'dabbawala-register',component: DabbawalaRegisterComponent},
   {path: 'user-login',component: UserLoginComponent},
-  {path: 'user-register',component: UserRegisterComponent},
   {path: 'home', component: HomeComponent},
   {path: 'about-us', component:AboutUsComponent},
   {path: 'dabba-service', component:DabbaServiceComponent},
@@ -36,6 +32,6 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-export const routingComponents = [DabbawalaLoginComponent, DabbawalaRegisterComponent, UserLoginComponent, UserRegisterComponent, HomeComponent,
+export const routingComponents = [DabbawalaLoginComponent, UserLoginComponent, HomeComponent,
    AboutUsComponent, DabbaServiceComponent, TedTalksComponent, DigitalDabbawalaComponent, CertificatesComponent, ImagesAndVideosComponent,PageNotFoundComponent,
   CentralisedKitchenComponent];
